7530aa143ca5169e45deb930ff5199376a223158
Kot <kot@yukata.dev>
2024-03-15 17:59:42 -0500

    Fix pagespeed issues, add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { visit } from "unist-util-visit";
-
+import sitemap from "@astrojs/sitemap";
 function remarkSkipHeading() {
 	return (tree) => {
 		visit(tree, "heading", (node) => {
@@ -16,13 +16,18 @@ export default defineConfig({
 		tailwind({
 			applyBaseStyles: false,
 		}),
+		sitemap(),
 	],
 	markdown: {
 		remarkPlugins: [remarkSkipHeading],
 	},
 	image: {
 		domains: ["avatars.githubusercontent.com", "cdn.erisa.uk", "joscomputing.space"],
-		remotePatterns: [{ protocol: "https" }],
+		remotePatterns: [
+			{
+				protocol: "https",
+			},
+		],
 	},
 	site: "https://kot.pink",
 });
